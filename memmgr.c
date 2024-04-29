94dbf6750e0070c11cb1a66c6c711eda7cba0ce0
hissongii <ssongii0@naver.com>
2024-04-29 14:18:55 +0900

    Update memmgr.c

--- memmgr.c
+++ memmgr.c
@@ -253,29 +253,25 @@ void mm_init(FreelistPolicy fp)
   // initialize heap
   //
   // TODO
-  // Initialize the heap
   size_t initial_heap_size = CHUNKSIZE;
   void* new_heap_segment = ds_sbrk(initial_heap_size);
   if (new_heap_segment == (void *)-1) {
       PANIC("Failed to extend heap.");
   }
 
-  // Set heap start and end
-  heap_start = new_heap_segment + (4 * WSIZE); // Start after prologue block and alignment padding
-  heap_end = new_heap_segment + initial_heap_size - (2 * WSIZE); // End before epilogue block
+  heap_start = new_heap_segment + (4 * WSIZE);
+  heap_end = new_heap_segment + initial_heap_size - (2 * WSIZE);
 
-  // Set prologue and epilogue
-  PUT(heap_start - (3 * WSIZE), PACK(DSIZE, 1)); // Prologue header
-  PUT(heap_start - (2 * WSIZE), PACK(DSIZE, 1)); // Prologue footer
+  PUT(heap_start - (3 * WSIZE), PACK(DSIZE, 1));
+  PUT(heap_start - (2 * WSIZE), PACK(DSIZE, 1));
 
-  PUT(heap_end, PACK(0, 1)); // Epilogue header
-
-  // Set the first free block
+  PUT(heap_end, PACK(0, 1));
+  /*
   size_t free_block_size = (heap_end - heap_start);
-  PUT(heap_start, PACK(free_block_size, 0)); // Free block header
-  PUT(heap_end - WSIZE, PACK(free_block_size, 0)); // Free block footer
-
-  mm_initialized = 1; // Mark heap as initialized
+  PUT(heap_start, PACK(free_block_size, 0));
+  PUT(heap_end - WSIZE, PACK(free_block_size, 0));
+  */
+  mm_initialized = 1;
     
 }
 
